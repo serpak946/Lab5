c30ff4988b0d41024f8f938d872e72ac1fa5ddec
serpak <zvinarevski946@yandex.com>
2022-04-09 23:17:41 +0300

    Добавьте файлы проекта.

---
+++ Lab5/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 537);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(590, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(282, 537);
+            this.panel3.TabIndex = 2;
+            this.panel3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseDown);
+            // 
+            // panel2
+            // 
+            this.panel2.AllowDrop = true;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(298, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(286, 537);
+            this.panel2.TabIndex = 1;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Lab5/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        int b_w = 0, b_h = 0;
+        int t_w = 0, t_h = 0;
+        int l_w = 0, l_h = 0;
+        int num_of_label = 1, num_of_button = 1, num_of_textbox = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        static void Change_Location(Button a, ref int w, ref int h, Panel panel)
+        {
+            a.Location = new Point(a.Location.X + w, a.Location.Y + h);
+            if (a.Location.X + 2 * a.Width + 2 * a.Margin.Right > panel.Width)
+            {
+                if (a.Location.Y + 2 * a.Height + 2 * a.Margin.Bottom < panel.Height)
+                {
+                    w = 0;
+                    h += a.Height + a.Margin.Top;
+                }
+            }
+            else w += a.Width + a.Margin.Left;
+        }
+        static void Change_Location(Label a, ref int w, ref int h, Panel panel)
+        {
+            a.Location = new Point(a.Location.X + w, a.Location.Y + h);
+            if (a.Location.X + 2 * a.Width + 2 * a.Margin.Right > panel.Width)
+            {
+                if (a.Location.Y + 2 * a.Height + 2 * a.Margin.Bottom < panel.Height)
+                {
+                    w = 0;
+                    h += a.Height + a.Margin.Top;
+                }
+            }
+            else w += a.Width + a.Margin.Left;
+        }
+        static void Change_Location(TextBox a, ref int w, ref int h, Panel panel)
+        {
+            a.Location = new Point(a.Location.X + w, a.Location.Y + h);
+            if (a.Location.X + 2 * a.Width + 2 * a.Margin.Right > panel.Width)
+            {
+                if (a.Location.Y + 2 * a.Height + 2 * a.Margin.Bottom < panel.Height)
+                {
+                    w = 0;
+                    h += a.Height + a.Margin.Top;
+                }
+            }
+            else w += a.Width + a.Margin.Left;
+        }
+
+        private void panel3_MouseDown(object sender, MouseEventArgs e)
+        {
+            Label l = new Label();
+            l.Parent = panel1;
+            l.Text = "Label "+Convert.ToString(num_of_label);
+            num_of_label++;
+            l.AutoSize = true;
+            Change_Location(l, ref l_w, ref l_h, panel1);
+        }
+
+        private void panel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            TextBox t = new TextBox();
+            t.Parent = panel3;
+            t.Text = "TextBox " + Convert.ToString(num_of_textbox);
+            num_of_textbox++;
+            Change_Location(t, ref t_w, ref t_h, panel3);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button b = new Button();
+            b.Parent = panel2;
+            b.Text = "Button " + Convert.ToString(num_of_button);
+            num_of_button++;
+            Change_Location(b, ref b_w, ref b_h, panel2);
+        }
+    }
+}
